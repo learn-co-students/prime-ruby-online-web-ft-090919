61bd75e16db71918ad452e82cd6e8c2b6ac27699
= <=>
2019-09-10 17:49:16 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -2,14 +2,8 @@ def prime?(x)
   #Must be greator than one
   if x <= 1 
     false
-  #dirty edge case
-  elsif x == 2
-    true
   else
-    #Check every possible divisor, up to half of x. If any of the are a factor it's not prime, otherwise it is
-    (2..x/2).each do |num|
-      return false if x % num == 0
-    end
-    true
+    #Check every possible divisor, up to half of x. If any of them are a factor, it isn't prime
+    (2..x/2).to_a.none? { |num| x % num == 0 }
   end
 end
