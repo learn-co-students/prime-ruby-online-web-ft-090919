24ef917888c23f16a4e94229463f1cb55c749a10
Doug Kintop <dkintop@hotmail.com>
2019-09-12 20:52:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+
+require 'pry'
+
+
+def prime?(integer)
+ 
+   if integer < 0 
+    integer = integer * -1 #changing - to +. idk if neccessary.
+  end
+  
+  if integer == 1 || integer == 0   
+    return false
+  #if statement deals with fringe cases that would return false positive
+  
+  
+  else
+  range = (2...integer)
+  !range.any? do |range_value|  
+    integer % range_value == 0
+  end
+  end
+end
+    
+    
+
