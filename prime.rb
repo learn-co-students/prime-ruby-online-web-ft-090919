677d5c601a07d0e77862921d2ee0a3b5c38df7d1
Dean L Betty <deanlloyd64@gmail.com>
2019-09-12 17:06:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    false
+  elsif num == 2 || num == 3
+    true
+  else
+    range = (2..(num - 1)).to_a
+    range.none? do |int|
+      num % int == 0
+    end
+  end
+end
