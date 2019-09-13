84da418812786fa2aa0683811c8c9735173205d1
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-13 15:42:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+    if number <= 1
+      return false
+    else
+        if (2...number).any? { |i| number % i  == 0}
+            return false
+        else
+            return true
+        end
+    end
+end
+
+ 
+   
+    
