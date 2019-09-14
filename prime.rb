3f96dc00d07a42f4180f1062913eaa4ddba81ff2
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-14 05:03:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  range = (2...num).to_a
+  if num <= 1
+    return false
+    
+  elsif num <= 3
+   return true
+    
+  else range.each{
+    |n| if num % n == 0
+   return false
+ end
+  }
+    end
+ true
+end
