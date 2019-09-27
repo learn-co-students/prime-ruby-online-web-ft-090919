c01d10a130faaba893a6c6e78e64f88866451200
Lialah Barrer <lrbarrer@gmail.com>
2019-09-27 21:26:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? { |possible_factor|  num % possible_factor != 0}
+  end
+end
